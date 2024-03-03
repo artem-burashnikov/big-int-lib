52782a3de6387a479416d8170369a13aebff3db8
Artem Burashnikov <artem.burashnikov@gmail.com>
2024-03-03 22:14:41 +0300

    Add mod

--- big-int-lib/mod.c
+++ big-int-lib/mod.c
@@ -1,2 +1,23 @@
+#include <stdint.h>
+
 #include "bigint.h"
 #include "utils.h"
+
+bigint_t *bigint_mod(const bigint_t *ap, const bigint_t *bp) {
+  bigint_t *r, *q, *qb;
+
+  r = q = qb = NULL;
+
+  if (!ap || !bp) {
+    return r;
+  }
+
+  q = bigint_div(ap, bp);
+  qb = bigint_mul(bp, q);
+  r = bigint_sub(ap, qb);
+
+  bifree(q);
+  bifree(qb);
+
+  return r;
+}
